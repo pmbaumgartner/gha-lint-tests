39e7b0cf491af4c0af6e187be390869799215624
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2022-08-22 10:28:17 -0400

    commit this file

---
+++ project/testme.py
@@ -0,0 +1,66 @@
+from .stuff import shit
+
+import functools
+
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+
+def func():
+    'Docstring.'
+    return 1
+    
+print("NO")
+
+# TODO: Wee
